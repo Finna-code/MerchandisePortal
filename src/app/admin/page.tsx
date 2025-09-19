cf6ae8716d02ed46871ef8ca39ec4bf23295599c
finna code <aman231023@gmail.com>
2025-09-20 05:22:42 +0530

    fixed auth issue

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,8 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+
+export const dynamic = 'force-dynamic';
+
 export default async function AdminPage() {
   const session = await auth();
   if (!session || (session as any).role !== "admin") redirect("/signin");
